440f8a72159b5243059d9a1c1be977f516570457
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-02 21:15:58 -0300

    edição das tarefas

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+
 import { NextPage } from 'next';
 import React from 'react';
 import {Task} from '../types/Task';
@@ -9,18 +11,19 @@ type ItemProps = {
 }
 
 export const Item:NextPage<ItemProps> = ({task, selectTaskToEdit}) => {
-
+    
     const geDataText = (finishDate : string | undefined, previsionDate : string) => {
         if(finishDate){
             return `Concluído em: ${moment(finishDate).format('DD/MM/yyyy')}`;
         }
-        return `Previsão de conclusão em: ${moment(previsionDate).format('DD/MM/yyyy')}`;
+        return `Previsão em: ${moment(previsionDate).format('DD/MM/yyyy')}`;
     }
-
+    
     return (
-        <div className={"container-item"+ (task.finishDate? "" : " ativo")}>
-            <img src={task.finishDate? '/checked.svg' : '/not-checked.svg'}
-                 alt={task.finishDate? 'Atividade Concluída' : 'Atividade Ativa'}/>
+        <div className={"container-item"+ (task.finishDate? "" : " ativo")}
+            onClick={_ => task.finishDate ? null : selectTaskToEdit(task)}>
+            <img className='img' src={task.finishDate? '/checked.svg' : '/not-checked.svg'}
+                alt={task.finishDate? 'Atividade Concluída' : 'Atividade Ativa'}/>
             <div>
                 <p className={task.finishDate? "concluido" : ""}>{task.name}</p>
                 <span>{geDataText(task.finishDate, task.previsionDate)}</span>
